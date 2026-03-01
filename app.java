5a256bd5adf939f1eb530d50725db426e8ee9ee6
Vedant Choubey <vedantchoubey@Vedants-MacBook-Air.local>
2026-03-01 15:55:59 +0530

    Added question 5

--- app.java
+++ app.java
@@ -1,16 +1,15 @@
 public class app {
+
     public static void main(String[] args) {
-
-        String[] banner = new String[5];
-
-        banner[0] = " *****   *****  *******  *****";
-        banner[1] = "*     * *     * *     *  *     ";
-        banner[2] = "*     * *     * *******  *****";
-        banner[3] = "*     * *     * *            *";
-        banner[4] = " *****   *****  *        *****";
-
-        for (int i = 0; i < banner.length; i++) {
-            System.out.println(banner[i]);
+        String[] banner = {
+            String.join("", " *** ", "  ***  ", "****** ","*******"),
+            String.join("", "*   *", " *   * ", "*    *", "*      "),
+            String.join("", "*   *", " *   * ", "******", "*******"),
+            String.join("", "*   *", " *   * ", "*     ", "      *"),
+            String.join("", " *** ", "  ***  ", "*     ", "*******")
+        };
+        for (String line : banner) {
+            System.out.println(line);
         }
     }
 }
