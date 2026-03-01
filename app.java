272a48774d7bc495631ffa943778a92185207988
Vedant Choubey <vedantchoubey@Vedants-MacBook-Air.local>
2026-03-01 14:39:25 +0530

    Clean base version

--- app.java
+++ app.java
@@ -1,57 +1,5 @@
-/*public class app {
-    public static void main(String[] args) {
-        
-        System.out.println(" *** " + "  *** " + " *** " + "  *** ");
-        System.out.println("*   *" + " *   *" + " *  *" + " *   *");
-        System.out.println("*   *" + " *   *" + " *  *" + " *    ");
-        System.out.println("*   *" + " *   *" + " *** " + "  *** ");
-        System.out.println("*   *" + " *   *" + " *   " + "     *");
-        System.out.println("*   *" + " *   *" + " *   " + " *   *");
-        System.out.println(" *** " + "  *** " + " *   " + " **** ");
-    } */
-
-
-
-/*public class app {
+public class app {
     public static void main(String[] args) {
         
         System.out.println("OOPS");
-    }*/
-
-
-
-/*public class OOPS {
-
-    public static void main(String[] args) {
-
-        System.out.println(String.join("   ",
-                "* * *",
-                "*   *",
-                "*   *",
-                "* * * "
-        ));
-
-        System.out.println(String.join("   ",
-                "* * *",
-                "*   *",
-                "*   *",
-                "* * *"
-        ));
-
-        System.out.println(String.join("   ",
-                "* * *",
-                "*   *",
-                "* * *",
-                "*    ",
-                "*    "
-        ));
-
-        System.out.println(String.join("   ",
-                "*****",
-                "*    ",
-                "*****",
-                "    *",
-                "*****"
-        ));
     }
-}*/
