e2fdc372a1f7abf828759c9fdae780f8d4ab6ae7
Vedant Choubey <vedantchoubey@Vedants-MacBook-Air.local>
2026-03-03 09:29:31 +0530

    level 1

--- app.java
+++ app.java
@@ -1,5 +1,25 @@
-public class app {
+//Level 1, Q1
+
+public class HarryAge {
     public static void main(String[] args) {
-        
-        System.out.println("OOPS");
+        int currentYear = 2024;
+        int birthYear = 2000;
+        int age = currentYear - birthYear;
+        System.out.println("Harry's age in 2024 is " + age);
     }
+}
+
+
+//Level 1, Q2
+
+public class SamMarks {
+    public static void main(String[] args) {
+        int maths = 94;
+        int physics = 95;
+        int chemistry = 96;
+        int totalMarks = maths + physics + chemistry;
+        double averageMark = (double) totalMarks / 3;
+        System.out.println("Sam's average mark in PCM is " + averageMark);
+    }
+}
+
